68ed804e99dd8facabcade3b371d67e75d34e2fd
hwangchanha <scorpi05@snu.ac.kr>
2024-04-03 17:02:04 +0900

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,35 @@
+def main():
+    print("Let's implement addition. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+
+    print("%d + %d = %d" % (x, y, add(x, y)))
+
+def add(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        print("%d / %d = %d" % (x, y, divide(x, y)))
+
+def divide(x, y):
+
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
+
+if __name__ == "__main__":
+    main()
